61be59c2e67a1ab205a14e1c86ab6c272a7bc4c1
Ajith M <98872197+ajithm7@users.noreply.github.com>
2022-04-06 14:49:50 +0530

    Update IntegerToRoman.java

--- IntegerToRoman.java
+++ IntegerToRoman.java
@@ -17,52 +17,91 @@ Input: num = 58
 Output: "LVIII"
 Explanation: L = 50, V = 5, III = 3.*/
 public class IntegerToRoman {
-	static Scanner  scanner=new Scanner(System.in);
+	static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		IntegerToRoman toRoman=new IntegerToRoman();
+		IntegerToRoman toRoman = new IntegerToRoman();
 		toRoman.solving();
 	}
 
 	private void solving() {
 		System.out.println("Enter your number : ");
-		 int k=scanner.nextInt();
-		 int n;
-		 if(k>=1000) {
-			  n=k/1000;
-			 while(n-->0) {
-			 System.out.print("M");}
-			 k=k-(k/1000*1000);
-		 }
-		 if(k<1000&&k>=500) {
-			 System.out.print("D");
-			 k-=500;
-		 }
-		 if(k<500&&k>=100) {
-			 n=k/100;
-			 while(n-->0) {
-			 System.out.print("C");}
-			 k=k-(k/100*100);
-		 }
-		 if(k<100&&k>=50) {
-			 System.out.print("L");
-			 k-=50;
-		 }
-		 if(k<50&&k>=10) {
-			 n=k/10;
-			 while(n-->0) {
-			 System.out.print("X");}
-			 k=k-(k/10*10);
-		 }
-		 if(k<10&&k>=5) {
-			 System.out.print("V");
-			 k-=5;
-		 }
-		 if(k<5&&k>=1) {
-			 while(k-->0) {
-			 System.out.print("I");}
-			 
-		 }
+		int k = scanner.nextInt();//4999
+		int n;
+
+		if (k >= 1000) {
+			n = k / 1000;
+			while (n-- > 0) {
+				System.out.print("M");
+			}
+			k = k - (k / 1000 * 1000);
+		}
 		
+
+		if (k < 1000 && k >= 500) {
+			if (k >= 900) {
+				System.out.print("CM");
+				k -= 900;
+			} else {
+				System.out.print("D");
+				k -= 500;
+			}
+		}
+
+		if (k < 500 && k >= 100) {
+			if (k >= 400) {
+				System.out.print("CD");
+				k -= 400;
+			} else {
+				n = k / 100;
+				while (n-- > 0) {
+					System.out.print("C");
+				}
+				k = k - (k / 100 * 100);
+			}
+		}
+		if (k < 100 && k >= 50) {
+			if (k >= 90) {
+				System.out.print("XC");
+				k -= 90;
+			} else {
+				System.out.print("L");
+				k -= 50;
+			}
+		}
+		if (k < 50 && k >= 10) {
+			if (k >= 40) {
+				System.out.print("XL");
+				k -= 40;
+			} else {
+				n = k / 10;
+				while (n-- > 0) {
+					System.out.print("X");
+				}
+				k = k - (k / 10 * 10);
+			}
+		}
+		if (k < 10 && k >= 5) {
+			if (k >= 9) {
+				System.out.print("IX");
+				k -= 9;
+			} else {
+				System.out.print("V");
+				k -= 5;
+			}
+		}
+		if (k < 5 && k >= 1) {
+			if (k >= 4) {
+				System.out.print("IV");
+				k -= 4;
+			} else {
+				while (k-- > 0) {
+					System.out.print("I");
+				}
+			}
+
+		}
+
 	}
 
 }
